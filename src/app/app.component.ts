23054219a45a36a9605514d2e212cdbf25fed412
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-20 12:52:19 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fes-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fe-stats-ng';
+}
