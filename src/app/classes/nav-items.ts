c13a1993f47b65b8d61c922b42218d96187a6772
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-21 11:49:58 -0700

    (nav): added router outlet, nav links work

--- src/app/classes/nav-items.ts
+++ src/app/classes/nav-items.ts
@@ -3,19 +3,19 @@ import { INavItem } from "../interfaces/nav-item";
 export class NavItems {
     private list:INavItem[] = [{
         title: 'Home',
-        url: ''
+        url: 'main'
     },{
-        title: 'Game Ststa',
-        url: ''
+        title: 'FF4:FE Stats',
+        url: 'fe-stats'
     },{
         title: 'Player Stats',
-        url: ''
+        url: 'player-stats'
     },{
         title: 'Player Rankings',
-        url: ''
+        url: 'player-rankings'
     }, {
         title: 'Current Races',
-        url: ''
+        url: 'current-races'
     }];
 
     public getItems():INavItem[] {
