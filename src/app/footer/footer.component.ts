a0af5215024a9583fbdd3d851fbccd7a56b7e32a
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-21 12:25:45 -0700

    footer complete

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavItems } from '../classes/nav-items';
+import { INavItem } from '../interfaces/nav-item';
 
 @Component({
   selector: 'fes-footer',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private navItems:NavItems = new NavItems();
+  public navList:INavItem[];
+
   constructor() { }
 
   ngOnInit() {
+    this.navList = this.navItems.getItems();
   }
 
 }
