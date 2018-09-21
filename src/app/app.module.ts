6c0be4f780685a9a1cb147712459a70275ad01b5
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-21 11:31:27 -0700

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './components/main/main.component';
+import { PlayerStatsComponent } from './components/player-stats/player-stats.component';
+import { PlayerRankingsComponent } from './components/player-rankings/player-rankings.component';
+import { FeStatsComponent } from './components/fe-stats/fe-stats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    MainComponent,
+    PlayerStatsComponent,
+    PlayerRankingsComponent,
+    FeStatsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/fe-stats/fe-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fes-fe-stats',
+  templateUrl: './fe-stats.component.html',
+  styleUrls: ['./fe-stats.component.scss']
+})
+export class FeStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
