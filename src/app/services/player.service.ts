36e94c32ca16fbc9b13582cf70afac3896d518b9
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-25 16:26:38 -0700

    created leaderboard methods for player service, but needs testing

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
 
 @Injectable()
 
@@ -13,6 +14,22 @@ export class PlayerService {
 
     public getRaceHistory(player:string) {
         return this._http.get(`http://api.speedrunslive.com/pastraces?player=${player}&game=ff4hacks&page=1&pageSize=500`);
+    }
+
+    public getLeaderBoard() {
+        return this._http.get(`http://api.speedrunslive.com/leaderboard/ff4hacks?season=0`);
+    }
+
+    public getRating(player: string) {
+        return this._http.get(`http://api.speedrunslive.com/leaderboard/ff4hacks?season=0`).pipe(
+            map(response => {
+                let rating = 0;
+                response['leaders'].forEach(leader => {
+                    if (leader['name'].toLowerCase() === player.toLowerCase()) rating = leader.trueskill;
+                })
+                return rating;
+            })
+        )
     }
 
 }
