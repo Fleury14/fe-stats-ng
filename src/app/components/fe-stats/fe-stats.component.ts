804bc4ecd80e4d4d92587c02594a944281a96793
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-10-01 16:44:30 -0700

    all races gotten, divided up by array. also created winning time cmp

--- src/app/components/fe-stats/fe-stats.component.ts
+++ src/app/components/fe-stats/fe-stats.component.ts
@@ -9,13 +9,47 @@ import { RaceService } from '../../services/race.service';
 export class FeStatsComponent implements OnInit {
 
   public stats;
+  public allRaces;
+  public racetypes = {
+    qual: [],
+    ro32: [],
+    ro16: [],
+    community: []
+  }
 
   constructor(private _race: RaceService) { }
 
   ngOnInit() {
     this._race.getStats().subscribe(resp => {
       this.stats = resp;
+    });
+    this._race.getAllRaces().subscribe(resp => {
+      this.allRaces = resp['pastraces'];
+      this.allRaces.forEach(race => {
+        if (race.goal.indexOf('J2KC2T4S3BF2NE3$X2Y2GWZ') !== -1) this.racetypes.qual.push(race);
+        if (race.goal.indexOf('JK2PCT3S2BF2NE3X2Y2GZ') !== -1) this.racetypes.ro32.push(race);
+        if (race.goal.indexOf('JK2PC3T3S2BF2NE3X2Y2GZ') !== -1) this.racetypes.ro16.push(race);
+        if (race.goal.indexOf('Community') !== -1) this.racetypes.community.push(race);
+      })
     })
+  }
+
+  private winningTimeCmp(race1, race2) {
+    
+      let race1Time = null;
+      let race2Time = null;
+      race1.results.forEach(result => {
+        if (result.place === 1) race1Time = result.time;
+      });
+      race2.results.forEach(result => {
+        if (result.place === 1) race2Time = result.time;
+      });
+      
+      race1Time = race1Time !== -1 ? race1Time : 99999;
+      race2Time = race2Time !== -1 ? race2Time : 99999;
+
+      return race1Time - race2Time;
+  
   }
 
 }
