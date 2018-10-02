812bfb997683edfe35437b7429274c8c7d72176b
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-10-02 13:52:24 -0700

    flags actually parse

--- src/app/components/fe-stats/fe-stats.component.ts
+++ src/app/components/fe-stats/fe-stats.component.ts
@@ -35,11 +35,14 @@ export class FeStatsComponent implements OnInit {
         if (race.goal.indexOf('JK2PC3T3S2BF2NE3X2Y2GZ') !== -1) this.racetypes.ro16.push(race);
         if (race.goal.indexOf('Community') !== -1) this.racetypes.community.push(race);
         this._race.addZScore(race);
+        this._parseRaceFlags(race);
       });
       this.racetypes.qual.sort(this.winningTimeCmp);
       this.racetypes.ro32.sort(this.winningTimeCmp);
       this.racetypes.ro16.sort(this.winningTimeCmp);
-    })
+      console.log('flags:', this.raceFlags);
+
+    });
   }
 
   private winningTimeCmp(race1, race2) {
@@ -65,66 +68,66 @@ export class FeStatsComponent implements OnInit {
     let seed_pos = race.goal.indexOf('&amp;seed=');
     let goal_flags = null;
     if (flags_pos !== -1 ) {
-        goal_flags = race.goal.slice( flags_pos + 7, seed_pos - flags_pos - 7);
+        goal_flags = race.goal.slice( flags_pos + 7, seed_pos);
     }
     
     if (goal_flags) { 
-      if ( goal_flags.indexOf('J') > -1 && goal_flags.indexOf('J2') === -1 ) { this.raceFlags['J'][1]++; this.raceFlags['J']['count']++; }
-      if ( goal_flags.indexOf('J2') > -1 ) { this.raceFlags['J'][2]++; this.raceFlags['J']['count']++; }
-      if ( goal_flags.indexOf('J') === -1 ) { this.raceFlags['J'][0]++; this.raceFlags['J']['count']++; }
-      if ( goal_flags.indexOf('K') > -1 && goal_flags.indexOf('K2') === -1 && goal_flags.indexOf('K3') === -1 && goal_flags.indexOf('K4') === -1 ) { this.raceFlags['K'][1]++; this.raceFlags['K']['count']++; }
-      if ( goal_flags.indexOf('K2') > -1 ) { this.raceFlags['K'][2]++; this.raceFlags['K']['count']++; }
-      if ( goal_flags.indexOf('K3') > -1 ) { this.raceFlags['K'][3]++; this.raceFlags['K']['count']++; }
-      if ( goal_flags.indexOf('K4') > -1 ) { this.raceFlags['K'][4]++; this.raceFlags['K']['count']++; }
-      if ( goal_flags.indexOf('K') === -1 ) { this.raceFlags['K'][0]++; this.raceFlags['K']['count']++; }
-      if ( goal_flags.indexOf('C') > -1 && goal_flags.indexOf('C2') === -1 && goal_flags.indexOf('C3') === -1 ) { this.raceFlags['C'][1]++; this.raceFlags['C']['count']++; }
-      if ( goal_flags.indexOf('C2') > -1 ) { this.raceFlags['C'][2]++; this.raceFlags['C']['count']++; }
-      if ( goal_flags.indexOf('C3') > -1 ) { this.raceFlags['C'][3]++; this.raceFlags['C']['count']++; }
-      if ( goal_flags.indexOf('C') === -1 ) { this.raceFlags['C'][0]++; this.raceFlags['C']['count']++; }
-      if ( goal_flags.indexOf('P') > -1 && goal_flags.indexOf('P2') === -1 ) { this.raceFlags['P'][1]++; this.raceFlags['P']['count']++; }
-      if ( goal_flags.indexOf('P2') > -1 ) { this.raceFlags['P'][2]++; this.raceFlags['P']['count']++; }
-      if ( goal_flags.indexOf('P') === -1 ) { this.raceFlags['P'][0]++; this.raceFlags['P']['count']++; }
-      if ( goal_flags.indexOf('T') > -1 && goal_flags.indexOf('T2') === -1 && goal_flags.indexOf('T3') === -1 && goal_flags.indexOf('T4') === -1 && goal_flags.indexOf('T5') === -1 ) { this.raceFlags['T'][1]++; this.raceFlags['T']['count']++; }
-      if ( goal_flags.indexOf('T2') > -1 ) { this.raceFlags['T'][2]++; this.raceFlags['T']['count']++; }
-      if ( goal_flags.indexOf('T3') > -1 ) { this.raceFlags['T'][3]++; this.raceFlags['T']['count']++; }
-      if ( goal_flags.indexOf('T4') > -1 ) { this.raceFlags['T'][4]++; this.raceFlags['T']['count']++; }
-      if ( goal_flags.indexOf('T5') > -1 ) { this.raceFlags['T'][5]++; this.raceFlags['T']['count']++; }
-      if ( goal_flags.indexOf('S') > -1 && goal_flags.indexOf('S2') === -1 && goal_flags.indexOf('S3') === -1 && goal_flags.indexOf('S4') === -1 && goal_flags.indexOf('S5') === -1 ) { this.raceFlags['S'][1]++; this.raceFlags['S']['count']++; }
-      if ( goal_flags.indexOf('S2') > -1 ) { this.raceFlags['S'][2]++; this.raceFlags['S']['count']++; }
-      if ( goal_flags.indexOf('S3') > -1 ) { this.raceFlags['S'][3]++; this.raceFlags['S']['count']++; }
-      if ( goal_flags.indexOf('S4') > -1 ) { this.raceFlags['S'][4]++; this.raceFlags['S']['count']++; }
-      if ( goal_flags.indexOf('S5') > -1 ) { this.raceFlags['S'][5]++; this.raceFlags['S']['count']++; }
-      if ( goal_flags.indexOf('B') > -1 && goal_flags.indexOf('B2') === -1 ) { this.raceFlags['B'][1]++; this.raceFlags['B']['count']++; }
-      if ( goal_flags.indexOf('B2') > -1 ) { this.raceFlags['B'][2]++; this.raceFlags['B']['count']++; }
-      if ( goal_flags.indexOf('B') === -1 ) { this.raceFlags['B'][0]++; this.raceFlags['B']['count']++; }
-      if ( goal_flags.indexOf('F') > -1 && goal_flags.indexOf('F2') === -1 ) { this.raceFlags['F'][1]++; this.raceFlags['F']['count']++; }
-      if ( goal_flags.indexOf('F2') > -1 ) { this.raceFlags['F'][2]++; this.raceFlags['F']['count']++; }
-      if ( goal_flags.indexOf('F') === -1 ) { this.raceFlags['F'][0]++; this.raceFlags['F']['count']++; }
-      if ( goal_flags.indexOf('N') > -1 && goal_flags.indexOf('N2') === -1 ) { this.raceFlags['N'][1]++; this.raceFlags['N']['count']++; }
-      if ( goal_flags.indexOf('N2') > -1 ) { this.raceFlags['N'][2]++; this.raceFlags['N']['count']++; }
-      if ( goal_flags.indexOf('N') === -1 ) { this.raceFlags['N'][0]++; this.raceFlags['N']['count']++; }
-      if ( goal_flags.indexOf('E') > -1 && goal_flags.indexOf('E2') === -1 && goal_flags.indexOf('E3') === -1 && goal_flags.indexOf('E4') === -1 ) { this.raceFlags['E'][1]++; this.raceFlags['E']['count']++; }
-      if ( goal_flags.indexOf('E2') > -1 ) { this.raceFlags['E'][2]++; this.raceFlags['E']['count']++; }
-      if ( goal_flags.indexOf('E3') > -1 ) { this.raceFlags['E'][3]++; this.raceFlags['E']['count']++; }
-      if ( goal_flags.indexOf('E4') > -1 ) { this.raceFlags['E'][4]++; this.raceFlags['E']['count']++; }
-      if ( goal_flags.indexOf('E') === -1 ) { this.raceFlags['E'][0]++; this.raceFlags['E']['count']++; }
-      if ( goal_flags.indexOf('$') > -1 && goal_flags.indexOf('$2') === -1 && goal_flags.indexOf('$3') === -1 ) { this.raceFlags['$'][1]++; this.raceFlags['$']['count']++; }
-      if ( goal_flags.indexOf('$2') > -1 ) { this.raceFlags['$'][2]++; this.raceFlags['$']['count']++; }
-      if ( goal_flags.indexOf('$3') > -1 ) { this.raceFlags['$'][3]++; this.raceFlags['$']['count']++; }
-      if ( goal_flags.indexOf('$') === -1 ) { this.raceFlags['$'][0]++; this.raceFlags['$']['count']++; }
-      if ( goal_flags.indexOf('X') > -1 && goal_flags.indexOf('X2') === -1 ) { this.raceFlags['X'][1]++; this.raceFlags['X']['count']++; }
-      if ( goal_flags.indexOf('X2') > -1 ) { this.raceFlags['X'][2]++; this.raceFlags['X']['count']++; }
-      if ( goal_flags.indexOf('X') === -1 ) { this.raceFlags['X'][0]++; this.raceFlags['X']['count']++; }
-      if ( goal_flags.indexOf('Y') > -1 && goal_flags.indexOf('Y2') === -1 ) { this.raceFlags['Y'][1]++; this.raceFlags['Y']['count']++; }
-      if ( goal_flags.indexOf('Y2') > -1 ) { this.raceFlags['Y'][2]++; this.raceFlags['Y']['count']++; }
-      if ( goal_flags.indexOf('Y') === -1 ) { this.raceFlags['Y'][0]++; this.raceFlags['Y']['count']++; }
-      if ( goal_flags.indexOf('G') > -1 && goal_flags.indexOf('G2') === -1 ) { this.raceFlags['G'][1]++; this.raceFlags['G']['count']++; }
-      if ( goal_flags.indexOf('G') === -1 ) { this.raceFlags['G'][0]++; this.raceFlags['G']['count']++; }
-      if ( goal_flags.indexOf('W') > -1 && goal_flags.indexOf('W2') === -1 ) { this.raceFlags['W'][1]++; this.raceFlags['W']['count']++; }
-      if ( goal_flags.indexOf('W2') > -1 ) { this.raceFlags['W'][2]++; this.raceFlags['W']['count']++; }
-      if ( goal_flags.indexOf('W') === -1 ) { this.raceFlags['W'][0]++; this.raceFlags['W']['count']++; }
-      if ( goal_flags.indexOf('Z') > -1 && goal_flags.indexOf('Z2') === -1 ) { this.raceFlags['Z'][1]++; this.raceFlags['Z']['count']++; }
-      if ( goal_flags.indexOf('Z') === -1 ) { this.raceFlags['Z'][0]++; this.raceFlags['Z']['count']++; }    
+      if ( goal_flags.indexOf('J') > -1 && goal_flags.indexOf('J2') === -1 ) { this.raceFlags.flags['J'][1]++; this.raceFlags.flags['J']['count']++; }
+      if ( goal_flags.indexOf('J2') > -1 ) { this.raceFlags.flags['J'][2]++; this.raceFlags.flags['J']['count']++; }
+      if ( goal_flags.indexOf('J') === -1 ) { this.raceFlags.flags['J'][0]++; this.raceFlags.flags['J']['count']++; }
+      if ( goal_flags.indexOf('K') > -1 && goal_flags.indexOf('K2') === -1 && goal_flags.indexOf('K3') === -1 && goal_flags.indexOf('K4') === -1 ) { this.raceFlags.flags['K'][1]++; this.raceFlags.flags['K']['count']++; }
+      if ( goal_flags.indexOf('K2') > -1 ) { this.raceFlags.flags['K'][2]++; this.raceFlags.flags['K']['count']++; }
+      if ( goal_flags.indexOf('K3') > -1 ) { this.raceFlags.flags['K'][3]++; this.raceFlags.flags['K']['count']++; }
+      if ( goal_flags.indexOf('K4') > -1 ) { this.raceFlags.flags['K'][4]++; this.raceFlags.flags['K']['count']++; }
+      if ( goal_flags.indexOf('K') === -1 ) { this.raceFlags.flags['K'][0]++; this.raceFlags.flags['K']['count']++; }
+      if ( goal_flags.indexOf('C') > -1 && goal_flags.indexOf('C2') === -1 && goal_flags.indexOf('C3') === -1 ) { this.raceFlags.flags['C'][1]++; this.raceFlags.flags['C']['count']++; }
+      if ( goal_flags.indexOf('C2') > -1 ) { this.raceFlags.flags['C'][2]++; this.raceFlags.flags['C']['count']++; }
+      if ( goal_flags.indexOf('C3') > -1 ) { this.raceFlags.flags['C'][3]++; this.raceFlags.flags['C']['count']++; }
+      if ( goal_flags.indexOf('C') === -1 ) { this.raceFlags.flags['C'][0]++; this.raceFlags.flags['C']['count']++; }
+      if ( goal_flags.indexOf('P') > -1 && goal_flags.indexOf('P2') === -1 ) { this.raceFlags.flags['P'][1]++; this.raceFlags.flags['P']['count']++; }
+      if ( goal_flags.indexOf('P2') > -1 ) { this.raceFlags.flags['P'][2]++; this.raceFlags.flags['P']['count']++; }
+      if ( goal_flags.indexOf('P') === -1 ) { this.raceFlags.flags['P'][0]++; this.raceFlags.flags['P']['count']++; }
+      if ( goal_flags.indexOf('T') > -1 && goal_flags.indexOf('T2') === -1 && goal_flags.indexOf('T3') === -1 && goal_flags.indexOf('T4') === -1 && goal_flags.indexOf('T5') === -1 ) { this.raceFlags.flags['T'][1]++; this.raceFlags.flags['T']['count']++; }
+      if ( goal_flags.indexOf('T2') > -1 ) { this.raceFlags.flags['T'][2]++; this.raceFlags.flags['T']['count']++; }
+      if ( goal_flags.indexOf('T3') > -1 ) { this.raceFlags.flags['T'][3]++; this.raceFlags.flags['T']['count']++; }
+      if ( goal_flags.indexOf('T4') > -1 ) { this.raceFlags.flags['T'][4]++; this.raceFlags.flags['T']['count']++; }
+      if ( goal_flags.indexOf('T5') > -1 ) { this.raceFlags.flags['T'][5]++; this.raceFlags.flags['T']['count']++; }
+      if ( goal_flags.indexOf('S') > -1 && goal_flags.indexOf('S2') === -1 && goal_flags.indexOf('S3') === -1 && goal_flags.indexOf('S4') === -1 && goal_flags.indexOf('S5') === -1 ) { this.raceFlags.flags['S'][1]++; this.raceFlags.flags['S']['count']++; }
+      if ( goal_flags.indexOf('S2') > -1 ) { this.raceFlags.flags['S'][2]++; this.raceFlags.flags['S']['count']++; }
+      if ( goal_flags.indexOf('S3') > -1 ) { this.raceFlags.flags['S'][3]++; this.raceFlags.flags['S']['count']++; }
+      if ( goal_flags.indexOf('S4') > -1 ) { this.raceFlags.flags['S'][4]++; this.raceFlags.flags['S']['count']++; }
+      if ( goal_flags.indexOf('S5') > -1 ) { this.raceFlags.flags['S'][5]++; this.raceFlags.flags['S']['count']++; }
+      if ( goal_flags.indexOf('B') > -1 && goal_flags.indexOf('B2') === -1 ) { this.raceFlags.flags['B'][1]++; this.raceFlags.flags['B']['count']++; }
+      if ( goal_flags.indexOf('B2') > -1 ) { this.raceFlags.flags['B'][2]++; this.raceFlags.flags['B']['count']++; }
+      if ( goal_flags.indexOf('B') === -1 ) { this.raceFlags.flags['B'][0]++; this.raceFlags.flags['B']['count']++; }
+      if ( goal_flags.indexOf('F') > -1 && goal_flags.indexOf('F2') === -1 ) { this.raceFlags.flags['F'][1]++; this.raceFlags.flags['F']['count']++; }
+      if ( goal_flags.indexOf('F2') > -1 ) { this.raceFlags.flags['F'][2]++; this.raceFlags.flags['F']['count']++; }
+      if ( goal_flags.indexOf('F') === -1 ) { this.raceFlags.flags['F'][0]++; this.raceFlags.flags['F']['count']++; }
+      if ( goal_flags.indexOf('N') > -1 && goal_flags.indexOf('N2') === -1 ) { this.raceFlags.flags['N'][1]++; this.raceFlags.flags['N']['count']++; }
+      if ( goal_flags.indexOf('N2') > -1 ) { this.raceFlags.flags['N'][2]++; this.raceFlags.flags['N']['count']++; }
+      if ( goal_flags.indexOf('N') === -1 ) { this.raceFlags.flags['N'][0]++; this.raceFlags.flags['N']['count']++; }
+      if ( goal_flags.indexOf('E') > -1 && goal_flags.indexOf('E2') === -1 && goal_flags.indexOf('E3') === -1 && goal_flags.indexOf('E4') === -1 ) { this.raceFlags.flags['E'][1]++; this.raceFlags.flags['E']['count']++; }
+      if ( goal_flags.indexOf('E2') > -1 ) { this.raceFlags.flags['E'][2]++; this.raceFlags.flags['E']['count']++; }
+      if ( goal_flags.indexOf('E3') > -1 ) { this.raceFlags.flags['E'][3]++; this.raceFlags.flags['E']['count']++; }
+      if ( goal_flags.indexOf('E4') > -1 ) { this.raceFlags.flags['E'][4]++; this.raceFlags.flags['E']['count']++; }
+      if ( goal_flags.indexOf('E') === -1 ) { this.raceFlags.flags['E'][0]++; this.raceFlags.flags['E']['count']++; }
+      if ( goal_flags.indexOf('$') > -1 && goal_flags.indexOf('$2') === -1 && goal_flags.indexOf('$3') === -1 ) { this.raceFlags.flags['$'][1]++; this.raceFlags.flags['$']['count']++; }
+      if ( goal_flags.indexOf('$2') > -1 ) { this.raceFlags.flags['$'][2]++; this.raceFlags.flags['$']['count']++; }
+      if ( goal_flags.indexOf('$3') > -1 ) { this.raceFlags.flags['$'][3]++; this.raceFlags.flags['$']['count']++; }
+      if ( goal_flags.indexOf('$') === -1 ) { this.raceFlags.flags['$'][0]++; this.raceFlags.flags['$']['count']++; }
+      if ( goal_flags.indexOf('X') > -1 && goal_flags.indexOf('X2') === -1 ) { this.raceFlags.flags['X'][1]++; this.raceFlags.flags['X']['count']++; }
+      if ( goal_flags.indexOf('X2') > -1 ) { this.raceFlags.flags['X'][2]++; this.raceFlags.flags['X']['count']++; }
+      if ( goal_flags.indexOf('X') === -1 ) { this.raceFlags.flags['X'][0]++; this.raceFlags.flags['X']['count']++; }
+      if ( goal_flags.indexOf('Y') > -1 && goal_flags.indexOf('Y2') === -1 ) { this.raceFlags.flags['Y'][1]++; this.raceFlags.flags['Y']['count']++; }
+      if ( goal_flags.indexOf('Y2') > -1 ) { this.raceFlags.flags['Y'][2]++; this.raceFlags.flags['Y']['count']++; }
+      if ( goal_flags.indexOf('Y') === -1 ) { this.raceFlags.flags['Y'][0]++; this.raceFlags.flags['Y']['count']++; }
+      if ( goal_flags.indexOf('G') > -1 && goal_flags.indexOf('G2') === -1 ) { this.raceFlags.flags['G'][1]++; this.raceFlags.flags['G']['count']++; }
+      if ( goal_flags.indexOf('G') === -1 ) { this.raceFlags.flags['G'][0]++; this.raceFlags.flags['G']['count']++; }
+      if ( goal_flags.indexOf('W') > -1 && goal_flags.indexOf('W2') === -1 ) { this.raceFlags.flags['W'][1]++; this.raceFlags.flags['W']['count']++; }
+      if ( goal_flags.indexOf('W2') > -1 ) { this.raceFlags.flags['W'][2]++; this.raceFlags.flags['W']['count']++; }
+      if ( goal_flags.indexOf('W') === -1 ) { this.raceFlags.flags['W'][0]++; this.raceFlags.flags['W']['count']++; }
+      if ( goal_flags.indexOf('Z') > -1 && goal_flags.indexOf('Z2') === -1 ) { this.raceFlags.flags['Z'][1]++; this.raceFlags.flags['Z']['count']++; }
+      if ( goal_flags.indexOf('Z') === -1 ) { this.raceFlags.flags['Z'][0]++; this.raceFlags.flags['Z']['count']++; }    
     }
   }
 
