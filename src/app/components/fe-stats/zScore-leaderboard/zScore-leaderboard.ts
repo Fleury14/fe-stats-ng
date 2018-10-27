69ec8ae3da75afce62f7a07f65f71ad336cfe270
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-10-27 15:38:14 -0700

    one section displays

--- src/app/components/fe-stats/zScore-leaderboard/zScore-leaderboard.ts
+++ src/app/components/fe-stats/zScore-leaderboard/zScore-leaderboard.ts
@@ -7,10 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 export class ZScoreLeaderboard implements OnInit {
+    selected: string = 'lastWeek';
     @Input() data: any[];
 
     ngOnInit() {
         console.log('data recieved', this.data);
+        console.log(this.data[this.selected]);
     }
     
 }
