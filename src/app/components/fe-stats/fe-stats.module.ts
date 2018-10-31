14f14a2189754bccce5d31f9a32c87bd70cf3885
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-10-30 22:48:51 -0700

    gave festats its own module

---
+++ src/app/components/fe-stats/fe-stats.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ZScoreLeaderboard } from './zScore-leaderboard/zScore-leaderboard';
+import { MostRacesLeaderboard } from './most-races-leaderboard/most-races-leaderboard';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [ MostRacesLeaderboard, ZScoreLeaderboard ],
+    imports: [ CommonModule ]
+})
+
+export class FeStatsModule {}
---
+++ src/app/components/fe-stats/most-races-leaderboard/most-races-leaderboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'fes-most-races',
+    templateUrl: './most-races-leaderboard.html',
+    styleUrls: [ './most-races-leaderboard.scss' ]
+})
+
+export class MostRacesLeaderboard {
+    
+}
