e5791bf0b352452d4aaa93d0389c57d187112bb6
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-10-31 11:30:36 -0700

    pagination done

--- src/app/components/fe-stats/most-races-leaderboard/most-races-leaderboard.ts
+++ src/app/components/fe-stats/most-races-leaderboard/most-races-leaderboard.ts
@@ -7,15 +7,38 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 export class MostRacesLeaderboard implements OnInit {
-    selected: string = 'lastWeek'
+    selected: string = 'lastWeek';
+    page: number = 1;
+    PAGELENGTH = 30;
+    paginatedItems: any[] = [];
     @Input() data: any;
     
     ngOnInit() {
-        console.log('data recieved', this.data);
+        this._paginateData(this.data[this.selected]);
     }
 
     public switchActive(flag: string) {
         this.selected = flag;
+        this._paginateData(this.data[this.selected]);
+    }
+
+    public switchPage(page: number) {
+        this.page = page;
+    }
+
+    private _paginateData(data: any[]) {
+        this.paginatedItems = [];
+        console.log(data.length / this.PAGELENGTH);
+        for (let i = 0; i < Math.ceil(data.length / this.PAGELENGTH); i++) {
+            this.paginatedItems.push([]);
+            for (let itemIndex = this.PAGELENGTH * i; itemIndex < this.PAGELENGTH * (i + 1); itemIndex++) {
+                if ( itemIndex < data.length ) {
+                    console.log('item index ', itemIndex, ' going into ', i);
+                    this.paginatedItems[i].push(data[itemIndex]);
+                }
+            }
+        }
+        console.log('paginated data', this.paginatedItems);
     }
     
 }
