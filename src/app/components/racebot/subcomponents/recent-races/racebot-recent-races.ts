93fdf0014c1be21b4179c8e86973f3190f749325
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-04-13 21:05:06 -0700

    feat(most recent): most recent races

--- src/app/components/racebot/subcomponents/recent-races/racebot-recent-races.ts
+++ src/app/components/racebot/subcomponents/recent-races/racebot-recent-races.ts
@@ -10,12 +10,25 @@ export class RacebotRecentRacesComponent {
     private NO_OF_RECENT_RACES = 10;
     public recentRaces: any[] = [];
     @Input() data: any;
+    selected: Number;
 
     ngOnInit() {
         for (let i = 0; i < this.NO_OF_RECENT_RACES; i++) {
             this.recentRaces.push(this.data[i]);
         }
+
+        for (let race of this.recentRaces) {
+            this.sortFinishersByPlacement(race.details.finishers)
+        }
         console.log('recent races', this.recentRaces);
+    }
+
+    public selectRace(num: Number) {
+        this.selected = num;
+    }
+
+    private sortFinishersByPlacement(finishers: any[]) {
+        return finishers.sort((finisher1, finisher2) => finisher1.placement - finisher2.placement);
     }
 
 }
